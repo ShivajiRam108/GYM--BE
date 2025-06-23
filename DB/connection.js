1eb0773d2582728b1d84b225add84d909cec83c4
ShivajiRam108-github <ramudus684@gmail.com>
2025-06-23 16:54:10 +0530

    change mangodb url

--- DB/connection.js
+++ DB/connection.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/gymBackEnd');
+    await mongoose.connect('mongodb+srv://ramudus684:admin123@cluster0.uwajz9x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log("✅ MongoDB connected successfully");
   } catch (err) {
     console.error("❌ MongoDB connection failed", err);
